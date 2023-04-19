ae129aada4b0a9f854e216c060a35eb4a7c1ab73
bruno <A01367102@itesm.mx>
2023-04-18 19:11:34 -0600

    REINICIE EL PROYECTO JAJA

--- DJANGO/facialreq/facialreq/urls.py
+++ DJANGO/facialreq/facialreq/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for storefront project.
+URL configuration for facialreq project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-import debug_toolbar
+from django.urls import path
+from facialreq.views import home 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls')),
-    path('^__debug__/', include(debug_toolbar.urls)),
+    path('', home),
 ]
--- DJANGO/playground/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-#urlconf
-urlpatterns = [
-    path('hello/', views.say_hello)
-]
