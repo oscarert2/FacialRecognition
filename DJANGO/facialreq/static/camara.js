29a651e2d0f9cc293353745de7ff990bcc03bb8a
BrunoMdz <brunomdz@hotmail.com>
2023-05-02 17:56:08 -0600

    cdxdx

--- DJANGO/facialreq/static/camara.js
+++ DJANGO/facialreq/static/camara.js
@@ -2,10 +2,20 @@ let camera_button = document.querySelector("#start-camera");
 let video = document.querySelector("#video");
 let click_button = document.querySelector("#click-photo");
 let canvas = document.querySelector("#canvas");
+let stop_camera_button = document.querySelector("#stop-camera");
 
 camera_button.addEventListener('click', async function() {
    	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
 	video.srcObject = stream;
+});
+
+stop_camera_button.addEventListener('click', function() {
+	if (stream) {
+        stream.getTracks().forEach(track => {
+            track.stop();
+        });
+        video.srcObject = null;
+    }
 });
 
 click_button.addEventListener('click', function() {
